595da5842b52efe264b69bdaaf478272f4d81e7a
5jiji <60753482+5jiji@users.noreply.github.com>
2023-06-07 22:30:42 +0200

    That's easier to read

--- src/events/dm.ts
+++ src/events/dm.ts
@@ -3,10 +3,8 @@ import { Server } from '../models/Server';
 import { Event } from './Event';
 
 export default new Event('messageCreate', async ($, event) => {
-  if (event.channel.type === 'DM') {
-    if (event.author.id === process.env.OWNERID) {
-      const servers = await Server.find();
-      await sendAnnouncement($, event, servers);
-    }
+  if (event.channel.type === 'DM' && event.author.id === process.env.OWNERID) {
+    const servers = await Server.find();
+    await sendAnnouncement($, event, servers);
   }
 });
